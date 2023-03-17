4a29fdb07727b11e6109529baf76e3b91d0eb060
shashi <shashipolity@gmail.com>
2023-03-17 21:38:47 +0530

    files uploaded

---
+++ demo.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from visa.exception import CustomException
+from visa.logger import logging
+import os, sys
+
+app = Flask(__name__)
+
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+    try:
+        raise Exception("We are testing custom exception")
+    except Exception as e:
+        visa = CustomException(e, sys)
+        logging.info(visa.error_message)
+        logging.info("We are testing logging file")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
