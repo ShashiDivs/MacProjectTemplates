5813b3ba626d14cf67c8f9ba0d795632c556ed10
shashi <shashipolity@gmail.com>
2023-03-31 09:12:52 +0530

    DataIngestion Done

--- visa/constant/__init__.py
+++ visa/constant/__init__.py
@@ -1,3 +1,37 @@
+import os
+from datetime import datetime
 
-DATA_INGESTION_CONFIG_KEY = 'data_ingestion_config'
-DATA_INGESTION_DOWNLOAD_URL_KEY = 'dataset_download_url'
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+ROOT_DIR = os.getcwd() # to get current working dir
+CONFIG_DIR = "config"
+CONFIG_FILE_NAME = "config.yaml"
+
+CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
+
+# DataIngested Related Variables: 
+DATA_INGESTION_CONFIG_KEY = "data_ingestion_config"
+DATA_INGESTION_DOWNLOAD_URL_KEY = "dataset_download_url"
+DATA_INGESTION_RAW_DATA_DIR_KEY = "raw_data_dir"
+DATA_INGESTION_INGESTED_DIR_KEY = "ingested_dir"
+DATA_INGESTION_INGESTED_TRAIN_DIR_KEY = "ingested_train_dir"
+DATA_INGESTION_INGESTED_TEST_DIR_KEY = "ingested_test_dir"
+
+
+# Training pipeline related varibales:
+TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
+TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
+TRAINING_PIPELINE_NAME_KEY = "pipeline"
+
+
+
+# Data validation Related varibales:
+
+
+# Data Transformation related varibales:
+
+
+# Model Training related varibales:
